5c1fe2466e44e0a15da496f8173c6a016565e45a
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-07-27 18:20:09 +0200

    show category ok

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -6,11 +6,15 @@
     <div class="container">
         <h1>{{ $category->name }}</h1>
         <ul class="my-5">
-            @foreach ($category->posts as $post)
+            @forelse ($category->posts as $post)
                 <li>
                     <a href="{{ route('admin.posts.show', $post->id) }}">{{ $post->title }}</a>
                 </li>
-            @endforeach
+            @empty
+                <li>
+                    <p>nessuna categoria</p>
+                </li>
+            @endforelse
         </ul>
     </div>
 @endsection
