ee92316a4bf8ce952b548527809d1f03e07e8d7c
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-07-30 15:39:18 +0200

    api ok in get

--- resources/js/front.js
+++ resources/js/front.js
@@ -2,6 +2,10 @@ window.Vue = require('vue');
 
 import App from './App.vue';
 
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 const app = new Vue(
     {
         el:'#root',
